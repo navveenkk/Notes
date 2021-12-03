1fb0b11c22e34b4302c3a549d9d8c32aa14f01e3
navveenkk <navveenkk@gmail.com>
2021-12-03 13:23:47 +0530

    Changed some scripts

--- script.js
+++ script.js
@@ -18,25 +18,25 @@ function validateField() {
               myFunction(this);
           }
       };
-      xhttp.open("POST", "data.xml", true);
+      xhttp.open("GET", "data.xml", true);
       xhttp.send();
   }
       
   function myFunction(xml) {
           var x,lcase,spc, i, txt, xmlDoc, term; 
-          lcase = document.getElementById("myInput");
+          lcase = document.getElementById("input");
           spc = lcase.value.toLowerCase();
           term = spc.split(' ').join('_');
           xmlDoc = xml.responseXML;
           txt = "search results for" + lcase;
           x = xmlDoc.getElementsByTagName(term);
           for (i = 0; i < x.length; i++) { 
-          txt += "<post>"+"<topic class=tpc>"+x[i].getElementsByTagName("topic")[0].childNodes[0].nodeValue + "</topic>"+ "<definition>" +
-          x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue + "</definition>" + "<hashtags>" + x[i].getElementsByTagName("hashtags")[0].childNodes[0].nodeValue + "</hashtags>" + "</post>";
+          txt += "<div class=post ocVdi8u>"+"<div class=topic ISdcf3h>"+x[i].getElementsByTagName("topic")[0].childNodes[0].nodeValue +"</div>"+"<div class=def Nbj6Jkli>" +
+          x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue + "</div>" + "<div class=tags Ooyh3vIb>" + x[i].getElementsByTagName("hashtags")[0].childNodes[0].nodeValue + "</div>" + "</div>";
           }
-          document.getElementById("demo").innerHTML = txt;
+          document.getElementById("notes").innerHTML = txt;
       
-      }
+  }
       
       
   
