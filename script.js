b3e4ed7607118051a27b830fe6442a44f263e5a9
navveenkk <navveenkk@gmail.com>
2021-12-03 12:23:17 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function validateField() {
+  var x = document.getElementById("myInput").value;
+  if (x == "") {
+    document.getElementById("b1").style.visibility="hidden";
+  } else {
+    document.getElementById("b1").style.visibility="visible";
+  }
+
+    
+    }
+
