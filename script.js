d48be6d33e9592346d0e257a69335a0f7480c55f
navveenkk <navveenkk@gmail.com>
2021-12-03 12:40:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,36 @@ function validateField() {
   }
 
     
-    }
+}
 
+
+  function loadSearch(){
+      var xhttp;
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+              myFunction(this);
+          }
+      };
+      xhttp.open("POST", "data.xml", true);
+      xhttp.send();
+  }
+      
+  function myFunction(xml) {
+          var x,lcase,spc, i, txt, xmlDoc, term; 
+          lcase = document.getElementById("myInput");
+          spc = lcase.value.toLowerCase();
+          term = spc.split(' ').join('_');
+          xmlDoc = xml.responseXML;
+          txt = "search results for" + lcase;
+          x = xmlDoc.getElementsByTagName(term);
+          for (i = 0; i < x.length; i++) { 
+          txt += "<post>"+"<topic class=tpc>"+x[i].getElementsByTagName("topic")[0].childNodes[0].nodeValue + "</topic>"+ "<definition>" +
+          x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue + "</definition>" + "<hashtags>" + x[i].getElementsByTagName("hashtags")[0].childNodes[0].nodeValue + "</hashtags>" + "</post>";
+          }
+          document.getElementById("demo").innerHTML = txt;
+      
+      }
+      
+      
+  
