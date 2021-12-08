f2b427cd6edda3432f97b4dda4ee33f23702bc47
Navveen K K <navveenkk@gmail.com>
2021-12-08 16:51:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+function loadSearch(){
+var xhttp;
+xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+    }
+};
+xhttp.open("GET", "data.xml", true);
+xhttp.send();
+}
+
+function myFunction(xml) {
+      var x,lcase,spc, i, txt, txta, txtb, xmlDoc, term; 
+          lcase = document.getElementById("input");
+          spc = lcase.value.toLowerCase();
+          term = spc.split(' ').join('_');
+          xmlDoc = xml.responseXML;
+          txt = "";
+          x = xmlDoc.getElementsByTagName(term);
+          for (i = 0; i < x.length; i++) { 
+          txt += "<div class=ocVdi8u id=posts>"+"<div class=ISdcf3h>"+x[i].getElementsByTagName("topic")[0].childNodes[0].nodeValue +"</div>"+"<div class=Nbj6Jkli>" +
+          x[i].getElementsByTagName("definition")[0].childNodes[0].nodeValue + "</div>"+"<div class=Ooyh3vIb>" + x[i].getElementsByTagName("hashtags")[0].childNodes[0].nodeValue + "</div>" + "</div>";
+          }
+          
+          txta="";
+          x = xmlDoc.getElementsByTagName(term);
+          for (i = 0; i < x.length; i++) { 
+          txta += "<div class=ocVdi8u>"+"<img src="+  x[i].getElementsByTagName("pics")[0].childNodes[0].nodeValue+ ">"+"</div>";
+          }
+          txtb="";
+          x = xmlDoc.getElementsByTagName(term);
+          for (i = 0; i < x.length; i++) { 
+          txtb += "<div class=ocVdi8u>"+"<iframe src=" + x[i].getElementsByTagName("video")[0].childNodes[0].nodeValue +" frameborder=0 allow=accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture allowfullscreen></iframe>"+"</div>";
+          }
+          txtc="";
+          x = xmlDoc.getElementsByTagName(term);
+          for (i = 0; i < x.length; i++) { 
+          txtc +=  "<img src="+  x[i].getElementsByTagName("pics")[0].childNodes[0].nodeValue+ ">";
+          }
+
+          document.getElementById("notes-container").innerHTML = txt + txta + txtb;
+          document.getElementById("notes-container").style.display = "";
+          document.getElementById("search-term").innerHTML = txt;
+
+          
+          
+         
 
 
+}
 
 function validateField() {  
             var restrictedWords = new Array("kill", "rape", "rapist", "fuck", "fucker", "fucking", "sex", "asshole", "fuck You", "dick", "bitch", "suck my dick", "ass hole");  
@@ -19,108 +68,10 @@ function validateField() {
             else {  
               document.getElementById("b1").style.visibility="visible";   
             }  
-        } 
-    
-
-        window.onscroll = function() {myScr()};
-    
-
-function myScr() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    
-      document.getElementById("side-container").className = "appr";
-      document.getElementById("search-box").appendChild(document.getElementById("logo-name"));
-      document.getElementById("search-box").appendChild(document.getElementById("search"));
-  }
-      else {
-    document.getElementById("side-container").className="si-co-hi";
-  document.getElementById("navigation-box").appendChild(document.getElementById("logo-name"));
-      document.getElementById("navigation-box").appendChild(document.getElementById("search"));
-    
-  } 
+            
+        }
   
-   
-}
-document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
-      t.style.left = n.clientX + "px", 
-		t.style.top = n.clientY + "px", 
-		e.style.left = n.clientX + "px", 
-		e.style.top = n.clientY + "px", 
-		i.style.left = n.clientX + "px", 
-		i.style.top = n.clientY + "px"
-    });
-    var t = document.getElementById("cursor"),
-        e = document.getElementById("cursor2"),
-        i = document.getElementById("cursor3");
-    function n(t) {
-        e.classList.add("hover", "hover-2"), i.classList.add("hover", "hover-2")
-    }
-    function s(t) {
-        e.classList.remove("hover", "hover-2"), i.classList.remove("hover", "hover-2")
-    }
-    s();
-    for (var r = document.querySelectorAll(".hover-target, .hover-target-2"), a = r.length - 1; a >= 0; a--) {
-        o(r[a])
-    }
-    function o(t) {
-        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
-    }
-	document.addEventListener('DOMContentLoaded', function () {
-	    var hoverLink = document.querySelector('.logo');
-	    var bodychange = document.querySelector('body');
-
-	    hoverLink.addEventListener('mouseenter', function (e) {
-	        bodychange.classList.add('logo-wrap');
-	    });
-	    hoverLink.addEventListener('mouseleave', function () {
-	        bodychange.classList.remove('logo-wrap');
-	    })
-	})
-	document.addEventListener('DOMContentLoaded', function () {
-	    var hoverLink = document.querySelector('.img-1');
-	    var bodychange = document.querySelector('body');
-
-	    hoverLink.addEventListener('mouseenter', function (e) {
-	        bodychange.classList.add('img-1-wrap');
-	    });
-	    hoverLink.addEventListener('mouseleave', function () {
-	        bodychange.classList.remove('img-1-wrap');
-	    })
-	})
-	document.addEventListener('DOMContentLoaded', function () {
-	    var hoverLink = document.querySelector('.img-2');
-	    var bodychange = document.querySelector('body');
-
-	    hoverLink.addEventListener('mouseenter', function (e) {
-	        bodychange.classList.add('img-2-wrap');
-	    });
-	    hoverLink.addEventListener('mouseleave', function () {
-	        bodychange.classList.remove('img-2-wrap');
-	    })
-	})
-	document.addEventListener('DOMContentLoaded', function () {
-	    var hoverLink = document.querySelector('.img-3');
-	    var bodychange = document.querySelector('body');
-
-	    hoverLink.addEventListener('mouseenter', function (e) {
-	        bodychange.classList.add('img-3-wrap');
-	    });
-	    hoverLink.addEventListener('mouseleave', function () {
-	        bodychange.classList.remove('img-3-wrap');
-	    })
-	})
-	document.addEventListener('DOMContentLoaded', function () {
-	    var hoverLink = document.querySelector('.img-4');
-	    var bodychange = document.querySelector('body');
-
-	    hoverLink.addEventListener('mouseenter', function (e) {
-	        bodychange.classList.add('img-4-wrap');
-	    });
-	    hoverLink.addEventListener('mouseleave', function () {
-	        bodychange.classList.remove('img-4-wrap');
-	    })
-	})
-
+      
       function coverRead() {
        var xhttp;
 xhttp = new XMLHttpRequest();
@@ -145,11 +96,90 @@ function myReads(xml) {
           document.getElementById("story-box").innerHTML = txt; 
       }
 
-
-        function SearchTerms(){
-          const words = ["psyche", "lakhimpur kheri"];
-          var x = document.getElementById("input").value;
-words.push(x);
-document.getElementById("t").innerHTML = words;
+      
+function searchform(inp, arr) {
+  var currentFocus;
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+  
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "searchform-list");
+      a.setAttribute("class", "searchform-items");
+      this.parentNode.appendChild(a);
+      for (i = 0; i < arr.length; i++) {
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          b = document.createElement("DIV");
+        
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.addEventListener("click", function(e) {
+              inp.value = this.getElementsByTagName("input")[0].value;
+              closeAllLists();
+          });
+          a.appendChild(b);
         }
- 
+      }
+  });
+
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "searchform-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {       
+        currentFocus++;
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        
+        currentFocus--;
+        
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        
+        e.preventDefault();
+        if (currentFocus > -1) {
+          
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    
+    if (!x) return false;
+    
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+  
+    x[currentFocus].classList.add("searchform-active");
+  }
+  function removeActive(x) {
+
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("searchform-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+  
+    var x = document.getElementsByClassName("searchform-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var countries = ["Psyche", "Lakhimpur Kheri"];
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+searchform(document.getElementById("input"), countries);
+
+
+
